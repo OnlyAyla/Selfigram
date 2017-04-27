6ccfb03f661cb64de352e54eb11d605854f314e2
OnlyAyla <lighthouselabs@admins-Mac-mini-3.local>
2017-04-26 19:17:42 -0700

    Custom Table View Cell
    Created a custom table view cell for the tab “Newsfeed”.
    Added in a “Camera” button that allows a new “Selfie” to be added

---
+++ SelfieCell.swift
@@ -0,0 +1,34 @@
+//
+//  SelfieCell.swift
+//  Selfigram
+//
+//  Created by lighthouselabs on 2017-04-26.
+//  Copyright © 2017 lighthouselabs. All rights reserved.
+//
+
+import UIKit
+
+class SelfieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var selfiImageView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
